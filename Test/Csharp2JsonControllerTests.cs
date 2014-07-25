f887bafd509addbc8f29da3f8a1877de668fff74
Joseph Flood <joseph.flood@hotmail.com>
2014-07-25 23:47:22 +1000

    removed strange apicontroller file

--- Test/Csharp2JsonControllerTests.cs
+++ Test/Csharp2JsonControllerTests.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using csharp2json;
+using Ploeh.AutoFixture;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using Xunit;
 
 namespace Test
 {
